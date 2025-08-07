5dc26c233ceaae73a0e6f389f9eee354c60c6b09
NuyoahCh <springoflotus@gmail.com>
2025-08-07 15:12:13 +0800

    Java并发线程基础

---
+++ src/concurrent/part02/MyThread.java
@@ -0,0 +1,18 @@
+package concurrent.part02;
+
+/**
+ * @author wangchen
+ * @version 1.0
+ */
+public class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("用继承 Thread 类的方式创建线程");
+    }
+
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+}
